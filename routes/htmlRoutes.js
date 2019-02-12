a3913d51b4e1047b54d009436962857c5ec1ea0d
Kaleigh Herold <kaleigh224@gmail.com>
2019-02-12 13:08:19 -0500

    finished api routes and got html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,14 @@
-var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
+  // Load index page 
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../views/home.handlebars"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load search results page 
+  app.get("/search/:id", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/layouts/landing.handlebars"))
   });
 
   // Render 404 page for any unmatched routes
