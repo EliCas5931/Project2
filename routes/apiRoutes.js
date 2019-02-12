f28ea962042047b47564685190eb8a9c15cc6a80
Kaleigh Herold <kaleigh224@gmail.com>
2019-02-12 10:52:38 -0500

    added GET and POST routes for search terms so far, still working on DELETE.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,38 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+module.exports = function (app) {
+  // Get all users
+  app.get("/api/search", function (req, res) {
+    db.Search.findAll({}).then(function (Project2) {
+      res.json(Project2);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Get route for returning a specific search term
+  app.get("/api/search/:searchTerm", function (req, res) {
+    db.Search.findAll({
+      where: {
+        searchTerm: req.params.searchTerm
+      }
+    })
+      .then(function (dbSearch) {
+        res.json(dbSearch);
+      });
+  });
+
+  // POST route to create search results
+  app.post("/api/search", function (req, res) {
+    db.Search.create({
+      searchTerm: req.body.searchTerm
+    })
+    .then(function(dbSearch) {
+      res.json(dbSearch);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
