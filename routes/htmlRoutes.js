e53bc9dbf76fce176811ca571ef951c3e8d00f68
Michael Howell <michael.wells.howell@gmail.com>
2019-02-13 09:06:21 -0500

    search page skeleton

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,12 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res){
+  app.get("/", function(req, res) {
     res.render("home");
+  })
+
+  app.get("/hello", function(req, res) {
+    res.render("search");
   })
 
   // Render 404 page for any unmatched routes
